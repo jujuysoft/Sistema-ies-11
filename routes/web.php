fa3c0813d016683f44401f286cdd9af7e312b3db
jesus <jv176118@gmail.com>
2025-08-18 11:53:57 -0300

    feat: Agregar entidad Anexo completa

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+//Rutas de Anexo
+
+
+
+
+
+
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
