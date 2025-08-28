264e3cc08408633da7f97fbacab72bdcb74b3e18
ezequielSaavedra02 <edezequiel20@gmail.com>
2025-08-28 14:31:21 -0300

    creacion de tabla ciclo lectivo

---
+++ app/Models/Icl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Icl extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'anio',
+        'fecha_inicio',
+        'fecha_fin',
+        'observaciones',
+    ];
+}
